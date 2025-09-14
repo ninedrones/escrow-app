9f90e4d1297c842c0bc7175016925f8489e331c6
ninedrones <tshinoda@protonmail.com>
2025-09-14 17:58:29 +0900

    fix hardhat setting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import '@nomicfoundation/hardhat-verify';
 import 'solidity-coverage';
 import 'dotenv/config';
 
